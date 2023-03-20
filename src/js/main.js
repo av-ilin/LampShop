662bcb5f1b5b3f19620c36c1c0025c592da50b18
Андрей Ильин <a.ilin.v@yandex.ru>
2023-03-20 18:37:57 +0300

    upd::lighter

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,24 @@ const lighter = document.getElementsByClassName("shop__interier-lighter")[0];
 
 let lightMode = true;
 
+function lighterCoord() {
+    let lampCoord = LampManager.elems.lampInterier.getBoundingClientRect();
+    let roomCoord = LampManager.elems.interier.getBoundingClientRect();
+    let lighterCoord = lighter.getBoundingClientRect();
+    return {
+        x:
+            lampCoord.x -
+            roomCoord.x +
+            lampCoord.width / 2 -
+            lighterCoord.width / 2,
+        y:
+            lampCoord.y -
+            roomCoord.y +
+            lampCoord.height * 0.9 -
+            lighterCoord.height / 2,
+    };
+}
+
 switchLight.onclick = () => {
     if (!lightMode) {
         switchLight.classList.add("active");
@@ -26,6 +44,10 @@ switchDark.onclick = () => {
         switchDark.classList.add("active");
         lighter.classList.add("active");
         LampManager.elems.interier.src = "images/interier_mydark.png";
+
+        let coord = lighterCoord();
+        lighter.style.left = `${coord.x}px`;
+        lighter.style.top = `${coord.y}px`;
         lightMode = false;
     }
 };
