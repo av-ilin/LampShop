ac0434bbea0e33a92d8c18e2ff9fec2537e9d57e
Андрей Ильин <a.ilin.v@yandex.ru>
2023-03-22 20:45:37 +0300

    add::burger

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,23 @@
 LampManager.update();
 Lighter.create();
+
+const nav = document.getElementsByClassName("nav")[0];
+const burger = document.getElementsByClassName("nav__burger")[0];
+burger.onclick = () => {
+    if (nav.classList.contains("burger")) {
+        document.body.style.overflow = "";
+        nav.classList.remove("burger");
+    } else {
+        document.body.style.overflow = "hidden";
+        nav.classList.add("burger");
+    }
+};
+
+window.addEventListener("resize", () => {
+    Lighter.upd();
+});
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 10) nav.classList.add("active");
+    else nav.classList.remove("active");
+});
